c836cb18ef9b5826ebc54746f647a6ad40a0a117
Fatemeh <fatemeh.ganbari1993@gmail.com>
2025-02-12 14:52:55 +0330

    initial project

---
+++ src/Library.API/Data/DatabaseInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+
+namespace Library.API.Data;
+
+public class DatabaseInitializer
+{
+    private IDbConnectionFactory _connectionFactory;
+
+    public DatabaseInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _connectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        await connection.ExecuteAsync(
+            @"CREATE TABE IF NOT EXISTS Books(
+            Isbn TEXT PRIMARY KEY,
+            Title TEXT NOT NULL,
+            Author TEXT NOT NULL,
+            ShortDescription TEXT NOT NULL,
+            PageCount INTEGER,
+            ReleaseDate TEXT NOT NULL,
+            )");
+    }
+}
---
+++ src/Library.API/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.API.Models;
+
+public class Book
+{
+    public required string Isbn { get; set; } 
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public required string ShortDescription { get; set; }
+    public int PageCount { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
