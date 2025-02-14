70118f5c09d8db6a292fc339cbe5b4a3f68b1074
Fatemeh <fatemeh.ganbari1993@gmail.com>
2025-02-14 16:29:59 +0330

    add service

--- src/Library.API/Program.cs
+++ src/Library.API/Program.cs
@@ -1,9 +1,25 @@
+using Library.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+    new SqliteConnectionFactory(builder.Configuration.GetValue<string>("Database:ConnectionString")));
+
+builder.Services.AddSingleton<DatabaseInitializer>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+// Db init here 
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.InitializeAsync();
 
 app.Run();
---
+++ src/Library.API/Services/BookService.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Library.API.Data;
+using Library.API.Models;
+
+namespace Library.API.Services;
+
+public class BookService : IBookService
+{
+    private readonly IDbConnectionFactory _connectionFactory;
+    public BookService(IDbConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task<bool> CreateAsync(Book book)
+    {
+        var existingBook = await GetByIsbnAsync(book.Isbn);
+        if (existingBook is not null)
+        {
+        }
+
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        await connection.ExecuteAsync(
+            @"INSERT INTO Books (Isbn, Ttile, Author, ShortDescription, PageCount, ReleaseDate)
+            VALUES (@Isbn, @Title, @Author, @ShortDescription, @PageCount, @ReleaseDate)", book);
+
+        return true;
+    }
+
+    public Task<bool> DeleteAsync(string isbn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Book>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book?> GetByIsbnAsync(string isbn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Book>> SearchbyTitleAsync(string searchTerm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+}
